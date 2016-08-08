502e5139a93e6c4f315ec1a5747cb9f29276f6f1
Rémi Parpaillon <remi.parpaillon@gmail.com>
2016-08-08 19:30:21 +0200

    Cell struct creation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+struct Cell {
+    x: i64,
+    y: i64,
+    alive: bool
+}
+
+impl Cell {
+}
+
 #[cfg(not(test))]
 fn main() {
     println!("Game of Life");
@@ -8,5 +17,14 @@ mod tests {
     #[test]
     fn it_works() {
         assert!(1 == 1);
+    }
+   
+    #[test]
+    fn cell_struct() {
+        use Cell;
+        let c = Cell {x: 1, y: 1, alive: true};
+        assert_eq!(true, c.alive);
+        assert_eq!(1, c.x);
+        assert_eq!(1, c.y);
     }
 }
