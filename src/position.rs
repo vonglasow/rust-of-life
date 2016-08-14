f55d7ba212b3e1063bde63231d5b445a54548a8a
Rémi Parpaillon <remi.parpaillon@gmail.com>
2016-08-14 23:04:10 +0200

    Add method Position::get_neighbors_positions
    This method from a given width and length return the neighbors positions
    of the current position.

--- src/position.rs
+++ src/position.rs
@@ -12,10 +12,28 @@ impl Position {
     pub fn create_2d (x: i16, y: i16) -> Position {
         return Position{x: x, y: y, z: 0};
     }
+
+    pub fn get_neighbors_positions_2d(&self, width: i16, length: i16) -> Vec<Position> {
+        let mut positions: Vec<Position> = vec![];
+        for i in -1..2 {
+            let y = self.y + i;
+            if y < 0 || y > length - 1 {
+                continue;
+            }
+            for j in -1..2 {
+                let x = self.x + j;
+                if x < 0 || x > width - 1 || (i == 0 && j == 0) {
+                    continue;
+                }
+                positions.push(Position::create_2d(x, y));
+            }
+        }
+        return positions;
+    }
 }
 
 #[cfg(test)]
-mod position_test {
+mod tests {
     #[test]
     fn test_create_2d() {
         use position::Position;
@@ -43,5 +61,22 @@ mod position_test {
         assert_eq!(true, position1 != position2);
         let position2 = Position {x: 1, y: 2, z: 2};
         assert_eq!(true, position1 != position2);
+    }
+
+    #[test]
+    fn test_get_neighbors_positions_2d() {
+        use position::Position;
+        let position1 = Position::create_2d(0, 0);
+        let neighbors = position1.get_neighbors_positions_2d(2, 2);
+        assert_eq!(3, neighbors.len());
+
+        assert_eq!(1, neighbors[0].x);
+        assert_eq!(0, neighbors[0].y);
+
+        assert_eq!(0, neighbors[1].x);
+        assert_eq!(1, neighbors[1].y);
+
+        assert_eq!(1, neighbors[2].x);
+        assert_eq!(1, neighbors[2].x);
     }
 }
