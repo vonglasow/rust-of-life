f7a012fc8af08834e3a10a519955aa574497c739
Rémi Parpaillon <remi.parpaillon@gmail.com>
2016-08-14 23:08:19 +0200

    Add method to print a World struct
    Refresh the terminal, and print the world and its current age.

--- src/printer.rs
+++ src/printer.rs
@@ -1,9 +1,10 @@
 use cell::Cell;
+use world::World;
 
 pub struct CliPrinter;
 
 impl CliPrinter {
-    pub fn print_cell(&self, cell: Cell) {
+    pub fn print_cell(cell: Cell) {
         use ansi_term::Style;
         use ansi_term::Color::Black;
         use ansi_term::Color::White;
@@ -14,23 +15,45 @@ impl CliPrinter {
         } else {
             color = White;
         }
-        println!("{}", Style::new().on(color).paint("  "));
+        print!("{}", Style::new().on(color).paint("  "));
+    }
+
+    pub fn print_world(world: &World) {
+        use std::process::Command;
+
+        println!("");
+        Command::new("clear").status();
+        println!("World age : {}", world.age);
+        for y in 0..world.cells.len() {
+            print!("\n");
+            for cell in &world.cells[y] {
+                CliPrinter::print_cell(Cell{ .. cell.clone()});
+            }
+        }
     }
 }
 
 #[cfg(test)]
-mod cli_printer_tests {
+mod tests {
     #[test]
     fn printer_print_cell() {
         use printer::CliPrinter;
         use cell::Cell;
         use position::Position;
 
-        let printer = CliPrinter;
         let position = Position::create_2d(1, 1);
-        let cell = Cell{position: position, alive: true};
-        printer.print_cell(cell);
-        let cell = Cell{position: position, alive: false};
-        printer.print_cell(cell);
+        let cell = Cell{position: position, alive: true, neighbors_positions: vec![]};
+        CliPrinter::print_cell(cell);
+        let cell = Cell{position: position, alive: false, neighbors_positions: vec![]};
+        CliPrinter::print_cell(cell);
+    }
+
+    #[test]
+    fn printer_print_world() {
+        use printer::CliPrinter;
+        use world::World;
+
+        let world = World::create_2d(10, 10);
+        CliPrinter::print_world(world);
     }
 }
