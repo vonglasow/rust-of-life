ff02ff272de0fa63ced27f23c97ea8fb40a002cc
Rémi Parpaillon <remi.parpaillon@gmail.com>
2016-08-16 01:11:12 +0200

    Add command line support

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 extern crate ansi_term;
 extern crate rand;
+extern crate clap;
 
 mod printer;
 mod position;
@@ -8,14 +9,29 @@ mod world;
 
 #[cfg(not(test))]
 fn main() {
-    use cell::Cell;
     use printer::CliPrinter;
-    use position::Position;
     use world::World;
-    use std::process::Command;
     use std::thread;
+    use clap::App;
 
-    let mut world = World::create_2d(20, 20);
+    let matches = App::new("Rust of life")
+        .version("v1.0-beta")
+         .args_from_usage("
+             <width> 'Width of the world'
+             <length> 'Length of the world'
+        ")
+        .get_matches();
+
+    let mut width: i16 = 20;
+    let mut length: i16 = 20;
+    if let Some(o) = matches.value_of("width") {
+        width = o.parse().unwrap();
+    }
+
+    if let Some(o) = matches.value_of("length") {
+        length = o.parse().unwrap();
+    }
+    let mut world = World::create_2d(width, length);
     CliPrinter::print_world(&world);
     for i in 0..1000 {
         world = world.next_gen();
